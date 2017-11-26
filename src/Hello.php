47e2c52bc4744f3ed704cf2cad21521614af5aa2
adamYxt <617003229@qq.com>
2017-11-26 15:19:45 +0800

    提交yii2 rabbitmq扩展基础结构

---
+++ src/Hello.php
@@ -0,0 +1,15 @@
+<?php
+namespace adamyxt\rabbitmq;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/26
+ * Time: 15:14
+ */
+class Hello
+{
+    public static function sayHello()
+    {
+        return "hello rabbitmq";
+    }
+}
